662fe272d9251809df9ebaeb9c727eadc353a8f9
Mariusz Szczepańczyk <m@mariusz.sh>
2025-01-26 23:37:19 +0100

    objectFromKeys

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ export function capitalize(str: string, locale = navigator.language): string {
   return str.replace(/^\p{CWU}/u, (char) => char.toLocaleUpperCase(locale));
 }
 
+export const objectFromKeys = <K extends string, V>(keys: readonly K[], value: (key: K) => V): { [key in K]: V } =>
+  keys.reduce((acc, key) => ({ ...acc, [key]: value(key) }), {}) as { [key in K]: V };
+
 export function sleep(ms: number): Promise<void> {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
